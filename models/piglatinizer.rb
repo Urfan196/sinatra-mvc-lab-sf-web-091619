f5cc9d833c19c3f9e74fe0ede87333d0f78796ec
Urfan Guliyev <urfan196@yahoo.com>
2019-10-08 18:28:44 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,7 +5,16 @@ attr_accessor :text
       @text = text
       end
 
-      def pig
-
+      def pig(word)
+        first_letter = word[0].downcase
+              if ["a", "e", "i", "o", "u"].include?(first_letter)
+                "#{word}way"
+              else
+                consonant_of_word = word.downcase.gsub(/[aeiou]/, ' ') .split(" ")
+                removed_consonant_legth = consonant_of_word.shift.length
+                splited_word = word.split('')
+                removed_word = splited_word.shift(removed_consonant_legth)
+                splited_word.join('') + removed_word.join('') + "ay"
+              end
       end
 end
