90c8d7388c4c12c282bc44c353ca1544e9cc7f7e
Urfan Guliyev <urfan196@yahoo.com>
2019-10-08 18:42:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,12 +3,16 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get'/' do
-
     erb :user_input
   end
 
-  post '' do
-    
+  post '/' do
+    erb :results
   end
+
+  post '/piglatinize' do
+        @analyzed_text = PigLatinizer.new
+        erb :piglatinize
+    end
 
 end
